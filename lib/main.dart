5141b908890b940e5e9bf5af23f1fd8cd78297f0
JAIKO12 <noufaltrianto@gmail.com>
2023-11-30 13:59:35 +0700

    penambahan validasi ke firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:aplikasi_pemesanan_makanan/atuhentication/authentication.dart';
+import 'package:aplikasi_pemesanan_makanan/firebase_options.dart';
 import 'package:aplikasi_pemesanan_makanan/login_page/welcome_page.dart';
 import 'package:aplikasi_pemesanan_makanan/login_page/theme_shared.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
